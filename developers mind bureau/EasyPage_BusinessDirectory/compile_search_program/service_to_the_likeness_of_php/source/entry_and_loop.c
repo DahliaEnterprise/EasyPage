9d7abc90da37b8df2b0275abba44128395c80a55
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-04-13 08:30:06 -0700

    Update entry_and_loop.c
    No compile syntax warning or errors no compile time errors or warning. No segment faults when a client connects, client successfully connects

--- developers mind bureau/EasyPage_BusinessDirectory/compile_search_program/service_to_the_likeness_of_php/source/entry_and_loop.c
+++ developers mind bureau/EasyPage_BusinessDirectory/compile_search_program/service_to_the_likeness_of_php/source/entry_and_loop.c
@@ -1,3 +1,4 @@
+
 //prerequisite
 #include <stdio.h>
 #include <sys/socket.h>
@@ -5,12 +6,25 @@
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <time.h>
+
+//nanosleep requires implicit header delcaration.
+int nanosleep(const struct timespec *req, struct timespec *rem);
 
 //global variables
 int server_socket_filedescriptor;
 
+  //constant time period
+  struct timespec time_to_suspend_shasta_exchange_iteration_remaining;
+  struct timespec time_to_suspend_shasta_exchange_iteration_request;
+
+
 //entry to program.
-void main()
+int main()
 {
   //initate socket
   server_socket_filedescriptor = -1;
@@ -19,8 +33,11 @@ void main()
     if(server_socket_filedescriptor == -1)
     {
       printf("socket() problems. \n");
-      return;
+      return -1;
     }
+    
+    //define server socket as non blocking.
+    //fcntl(server_socket_filedescriptor, F_SETFL, O_NONBLOCK);
     
   //initiate the server address details (majority)ram location storage.
   struct sockaddr_in server_address;
@@ -30,15 +47,16 @@ void main()
     //define parameters pertaining to "server_address".
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(8080);
-    
+    server_address.sin_port = htons(3456);
+    memset(server_address.sin_zero, 0, 8);
+   
   //bind (network inout)file with server address information. (alert the network card what to do with this file)
-  struct sockaddr * general_server_address_sizeof = (struct sockaddr*)malloc(sizeof(struct sockaddr));
-  int bind_return_code = bind(server_socket_filedescriptor, general_server_address_sizeof, sizeof(general_server_address_sizeof));
+  //struct sockaddr * general_server_address_sizeof = (struct sockaddr*)malloc(sizeof(struct sockaddr));
+  int bind_return_code = bind(server_socket_filedescriptor, (struct sockaddr *)&server_address, sizeof(struct sockaddr));
   if(bind_return_code != 0)
   {
-   printf("bind() \n");
-   return;
+   printf("bind() %s \n", strerror(errno));
+   return -2;
   }
   
   //enable listening functioning.
@@ -46,8 +64,39 @@ void main()
   if(listen_return_code != 0)
   {
     printf("listen() \n");
-    return;
+    return -3;
   }
   
-  
+  //begin shasta exchange iteration of trigger based automated work.
+    //initialize temporary holder for newly accepted connections.
+    int temporary_socket_file_descriptor = -1;
+    socklen_t length = 0;
+    temporary_socket_file_descriptor = accept(server_socket_filedescriptor, (struct sockaddr *)&server_address, &length);
+     
+    
+    struct sockaddr_in client_address_info;
+    
+    //do begin while loop
+    time_to_suspend_shasta_exchange_iteration_request.tv_nsec = 10000000;
+    
+    int shasta_exchange_iteration_continue_loop = 1;
+    while(shasta_exchange_iteration_continue_loop == 1)
+    {
+     //determine if a connection is awaiting to be accepted.
+     temporary_socket_file_descriptor = accept(server_socket_filedescriptor, (struct sockaddr *)&server_address, &length);
+     if(temporary_socket_file_descriptor < 0)
+     {
+      // printf("accept() %s \n", strerror(errno));
+       
+     }else{
+       printf("client detected. \n");
+     }
+       
+     //cpu gets hot during while loops with zero load.
+     nanosleep(&time_to_suspend_shasta_exchange_iteration_request, &time_to_suspend_shasta_exchange_iteration_remaining);
+    }
+    
+  return 0;
 }
+
+
