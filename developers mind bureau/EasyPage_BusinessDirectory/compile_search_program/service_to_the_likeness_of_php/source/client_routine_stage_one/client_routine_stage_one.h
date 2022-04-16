3f86f992dfe164badaec4cf24c61798a78dcb342
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-04-16 12:42:27 -0700

    intended version

---
+++ developers mind bureau/EasyPage_BusinessDirectory/compile_search_program/service_to_the_likeness_of_php/source/client_routine_stage_one/client_routine_stage_one.h
@@ -0,0 +1,1 @@
+void client_routine_stage_one(char temporary_receive_buffer[], const unsigned int receive_string_max_size, const int client_socket_filedescriptor, char * receive_buffer_associated_to_the_respective_client, char * whole_message_transmitted_flag, unsigned short int * the_current_stage_the_client_connection_is_active_in);
