cff5411fae47eccb3e4c38fd8fd989583ee63e24
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-04-17 07:11:16 -0700

    Update entry_and_loop.c

--- developers mind bureau/EasyPage_BusinessDirectory/compile_search_program/service_to_the_likeness_of_php/source/entry_and_loop.c
+++ developers mind bureau/EasyPage_BusinessDirectory/compile_search_program/service_to_the_likeness_of_php/source/entry_and_loop.c
@@ -19,14 +19,21 @@ int nanosleep(const struct timespec *req, struct timespec *rem);
 
 //shasta exchange
   //shasta exchange headers
-  #include "./produce_available_index_of_list_of_client_connection_slots/produce_available_index_of_list_of_client_connection_slots.h"
-  #include "./structure_string/structure_string.h"
-  #include "./client_routine_stage_one/client_routine_stage_one.h"
+    //independent structure(s)
+    #include "./structure_string/structure_string.h"
+    
+    //major functions
+    #include "./produce_available_index_of_list_of_client_connection_slots/produce_available_index_of_list_of_client_connection_slots.h"
+    #include "./client_routine_stage_one/client_routine_stage_one.h"
   
   //shasta exchange definitions
-  #include "./produce_available_index_of_list_of_client_connection_slots/produce_available_index_of_list_of_client_connection_slots.c"
-  #include "./structure_string/structure_string.c"
-  #include "./client_routine_stage_one/client_routine_stage_one.c"
+    //independent structure(s)
+    #include "./structure_string/structure_string.c"
+    
+    //major functions
+    #include "./extract_list_of_business_filenames_to_parseable_ram/extract_list_of_business_filenames_to_parseable_ram.c"
+    #include "./produce_available_index_of_list_of_client_connection_slots/produce_available_index_of_list_of_client_connection_slots.c"
+    #include "./client_routine_stage_one/client_routine_stage_one.c"
 
 //global variables
   //service connection
@@ -58,110 +65,19 @@ int main()
   //root_directory_of_content "/var/www/business_directory_content/";
   //"/var/www/business_directory_content/list_of_businesses/";
 
-  //have a list of filenames representing business names at tbe ready.
-  char * absolute_file_path_directory_list_of_businesses = "/var/www/business_directory_content/list_of_businesses/list_of_businesses.text";
-  FILE * list_of_businesses_dot_text = fopen(absolute_file_path_directory_list_of_businesses, "r");
-  long int total_characters = 0;
-  if(list_of_businesses_dot_text != NULL)
-  {
-    fseek(list_of_businesses_dot_text, 0, SEEK_END);
-    total_characters = ftell(list_of_businesses_dot_text);
-    fseek(list_of_businesses_dot_text, 0, SEEK_SET);
-   
-    //determine total new lines. ";" is the delimter.
-    char character[2] = "\0\0";
-    unsigned long int total_delimeters = 0;
-    unsigned long int list_of_businesses_dot_text_index = 0;
-    while(list_of_businesses_dot_text_index < total_characters)
+
+  //extract text file style list(characters) into a c style array of string names that which make it parseable.
+    //initialize nessecsary memory spaces.
+    struct string * list_of_business_records = 0;
+    
+    //do extract and convert to ram parseabme list.
+    int extract_business_name_list_return_code = extract_list_of_business_filenames_to_parseable_ram(list_of_business_records);
+    
+    if(extract_business_name_list_return_code < 1)
     {
-     fread(character, 1, 1, list_of_businesses_dot_text);
-     int character_matches = strncmp(character, ";", 1);
-     if(character_matches == 0)
-     {
-       total_delimeters = total_delimeters + 1;
-     }
-    
-      //next character within index.
-      list_of_businesses_dot_text_index = list_of_businesses_dot_text_index + 1;
+      return -1;
     }
-    
-    //extract as many lines as their are delimeters detected.
-      //rewind the proverbial tape.
-      fseek(list_of_businesses_dot_text, 0, SEEK_SET);
-  
-      //allocate as many string structures as their are total delemiters
-      struct string * list_of_business_records = 0;
-      while(list_of_business_records == 0){ list_of_business_records = (struct string *)malloc(total_delimeters * sizeof(struct string)); }
-    
-      //for each business filename, store that file name into the list of business records.
-      unsigned int start_of_business_filename = 0;
-      unsigned int end_of_business_filename = 0;
-      unsigned int list_of_business_records_index = 0;
-      list_of_businesses_dot_text_index = 0;
-      while(list_of_business_records_index < total_delimeters)
-      {
-        //keep reading until end of file or delmiter found.
-        unsigned short int keep_reading_character = 1;
-        while(keep_reading_character == 1)
-        {
-          fread(character, 1, 1, list_of_businesses_dot_text);
-          int character_matches = strncmp(character, ";", 1);
-          if(character_matches == 0)
-          {
-            //
-            //end_of_business_filename = end_of_business_filename + 1;
-            //list_of_businesses_dot_text_index = list_of_businesses_dot_text_index + 1;
-            
-            //extract filename
-            char * business_filename = 0;
-            unsigned int filename_length = end_of_business_filename - start_of_business_filename;
-            while(business_filename == 0){ business_filename = (char *)malloc((filename_length+1) * sizeof(char)); }
-            memset(business_filename, '\0', (filename_length+1));
-            fseek(list_of_businesses_dot_text, start_of_business_filename, SEEK_SET);
-            fread(business_filename, 1, filename_length, list_of_businesses_dot_text);
-         
-            //store filename to be used later.
-              //allocate.
-              list_of_business_records[list_of_business_records_index].string_buffer = 0;
-              while(list_of_business_records[list_of_business_records_index].string_buffer == 0){ list_of_business_records[list_of_business_records_index].string_buffer = (char *)malloc((filename_length+1) * sizeof(char)); }
-              memset(list_of_business_records[list_of_business_records_index].string_buffer, '\0', (filename_length+1));
-              list_of_business_records[list_of_business_records_index].string_buffer_memory_size = (filename_length+1);
-             
-              //store
-              strcat(list_of_business_records[list_of_business_records_index].string_buffer, business_filename);
-              
-            //next segment
-              //start past delimeter.
-              end_of_business_filename = end_of_business_filename + 2;
-              start_of_business_filename = end_of_business_filename;
-              
-              //read past the delimeter.
-              fread(character, 1, 2, list_of_businesses_dot_text);
-              
-              //break out of parse by character loop.
-              keep_reading_character = 0;
-              
-              //delete,free temporary variables.
-              free(business_filename);
-              
-          }else if(character_matches != 0)
-          {
-            end_of_business_filename = end_of_business_filename + 1;
-            
-          }
-        }
-        
-        //char * business_filename = readline(
-        
-        //next line
-        list_of_business_records_index = list_of_business_records_index + 1;
-      }
-    
-    
-  }else if(list_of_businesses_dot_text == NULL)
-  {
-    return 1;
-  } 
+
 
   //initate socket
   server_socket_filedescriptor = -1;
