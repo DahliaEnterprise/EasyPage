a6c8fb5df77f783a5d335dd92a6975211e2f0118
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-04-19 16:17:45 -0700

    Update entry_main_program.c
    I was unable to solve the memory. And or hard drive leaks as I began to have short term memory limitations. That is to say I could not live the interworkings of the program within my mind in order to fix it or change it.
    Change to c++ and using creator environment

--- developers mind bureau/EasyPage_BusinessDirectory/compile_search_program/service_to_compile_directory_metadata/source/create_directory/entry_main_program.c
+++ developers mind bureau/EasyPage_BusinessDirectory/compile_search_program/service_to_compile_directory_metadata/source/create_directory/entry_main_program.c
@@ -20,6 +20,7 @@ struct business_name_and_identification {
 struct business_id_and_per_character_location {
   unsigned long int business_identification_number;
   unsigned short int * per_character_sort_id;
+  unsigned short int per_character_sort_id_length;
   //unsigned short int * flag_character_slot_as_space; //this can be replaced using max detected string length.
   
 };
@@ -123,9 +124,6 @@ int main()
           //define business id to associate information.
           business_file_sort_information[list_of_business_records_index].business_identification_number = business_identification_number;
           
-          //initialize per character sort id.
-          business_file_sort_information[list_of_business_records_index].per_character_sort_id = 0;
-          while(business_file_sort_information[list_of_business_records_index].per_character_sort_id == 0){ business_file_sort_information[list_of_business_records_index].per_character_sort_id = (unsigned short int *)malloc(strlen(list_of_business_records[list_of_business_records_index].string_buffer) * sizeof(unsigned short int)); } memset(business_file_sort_information[list_of_business_records_index].per_character_sort_id, '\0', strlen(list_of_business_records[list_of_business_records_index].string_buffer));
         
           //open business file to define "per character sort id".
           absolute_path_and_filename = 0;
@@ -142,6 +140,11 @@ int main()
           printf("\n--------\n");
           free(absolute_path_and_filename);
           
+          //find the business sort latch content pair.
+          char * business_sort_title = 0;
+          unsigned short int latch_content_pair_found = 0;
+          while(latch_content_pair_found == 0)
+          {
             //determine how long the next latch and content value is.
             short int length_of_next_latch_and_content_line = extract_list_of_business_attributes_to_parseable_ram_step_one_determine_total_characters_of_line(business_attribute_file_dot_text);
               //rewind to starting position of filename.
@@ -149,23 +152,84 @@ int main()
                 long int current_position = ftell(business_attribute_file_dot_text);
                 //do set position to begining of filename.
                 fseek(business_attribute_file_dot_text , (current_position-length_of_next_latch_and_content_line), SEEK_SET);
-        /*
+                
+           
             //read then copy from storage to temporary string for parse/interpretation.
             temporary_latch_and_content_storage_string = 0;
-            while(temporary_latch_and_content_storage_string == 0){ temporary_latch_and_content_storage_string = (char *)malloc(length_of_next_latch_and_content_line * sizeof(char)); } memset(temporary_latch_and_content_storage_string, '\0', length_of_next_latch_and_content_line);
-            fread(temporary_latch_and_content_storage_string, 1, (length_of_next_latch_and_content_line-1), list_of_businesses_dot_text);
+            while(temporary_latch_and_content_storage_string == 0){ temporary_latch_and_content_storage_string = (char *)malloc((length_of_next_latch_and_content_line+1) * sizeof(char)); } memset(temporary_latch_and_content_storage_string, '\0', (length_of_next_latch_and_content_line+1));
+            fread(temporary_latch_and_content_storage_string, 1, (length_of_next_latch_and_content_line), business_attribute_file_dot_text);
          
-            printf("hjgf: %s\n", temporary_latch_and_content_storage_string);
+            
+            int compare_return_code = strncmp(temporary_latch_and_content_storage_string, "business_sort_title", 19);
+            if(compare_return_code == 0)
+            {
+              printf("%s\n", temporary_latch_and_content_storage_string);
+              
+              //isolate the content section of the latch content pair.
+              unsigned int start_isolate_index = 20;
+              unsigned int end_isolate_index = length_of_next_latch_and_content_line-1;
+              unsigned int business_sort_title_length = end_isolate_index - start_isolate_index;
+              business_sort_title = 0;
+              while(business_sort_title == 0){ business_sort_title = (char *)malloc((business_sort_title_length+1) * sizeof(char)); } memset(business_sort_title, '\0', (business_sort_title_length+1));
+              strncat(business_sort_title, temporary_latch_and_content_storage_string+start_isolate_index, business_sort_title_length);
+              
+              //determine the coresponding "per character id" based upon the "business sort title".
+                //initialize per character sort id.
+                business_file_sort_information[list_of_business_records_index].per_character_sort_id = 0;
+                while(business_file_sort_information[list_of_business_records_index].per_character_sort_id == 0){ business_file_sort_information[list_of_business_records_index].per_character_sort_id = (unsigned short int *)malloc((business_sort_title_length) * sizeof(unsigned short int)); } memset(business_file_sort_information[list_of_business_records_index].per_character_sort_id, '\0', (business_sort_title_length));
+                business_file_sort_information[list_of_business_records_index].per_character_sort_id_length = business_sort_title_length;
+                
+                //define from left to right the corresponding character id.
+                unsigned int per_character_id_index = 0;
+                while(per_character_id_index < business_sort_title_length)
+                {
+                  //loop through the character table to determine the character id
+                  unsigned short int character_table_index = 0;
+                  while(character_table_index < 26)
+                  {
+                    if(strncmp(character_table+character_table_index, business_sort_title+per_character_id_index, 1) == 0)
+                    {
+                      business_file_sort_information[list_of_business_records_index].per_character_sort_id[per_character_id_index] = character_table_index;
+                      printf("%hu\n", business_file_sort_information[list_of_business_records_index].per_character_sort_id[per_character_id_index]);
+                      
+                      //end search
+                      character_table_index = 26;
+                    }else{
+                      //next character to compare
+                      character_table_index = character_table_index + 1;
+                    }
+                  }
+                  
+                  //move on to next character id slot.
+                  per_character_id_index = per_character_id_index + 1;
+                }
+                
+                //free(business_sort_title);
+              printf("stop search\n");
+              
+               //read past the delimeter, prepares for the next round.
+               char character[1];
+               fread(character, 1, 1, business_attribute_file_dot_text);
+            
+              
+              //stop search.
+              latch_content_pair_found = 1;
+            }
+            
+            
             free(temporary_latch_and_content_storage_string);
-            */
-          
+           
+           //
+          }
           
           //temp end
-          list_of_business_records_index = list_of_business_records_total;
+          //list_of_business_records_index = list_of_business_records_total;
           
         //next business to assign characters number values.
         list_of_business_records_index = list_of_business_records_index + 1;
       }
+      
+      printf("temp\n");
   
   return output;   
 }
