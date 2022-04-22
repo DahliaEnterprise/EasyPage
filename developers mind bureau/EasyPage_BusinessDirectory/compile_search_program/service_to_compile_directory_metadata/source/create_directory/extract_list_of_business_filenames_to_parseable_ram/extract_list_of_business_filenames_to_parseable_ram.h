4f9117834aa447c543f3fea3f813984392485947
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-04-22 03:07:11 -0700

    Delete extract_list_of_business_filenames_to_parseable_ram.h

--- developers mind bureau/EasyPage_BusinessDirectory/compile_search_program/service_to_compile_directory_metadata/source/create_directory/extract_list_of_business_filenames_to_parseable_ram/extract_list_of_business_filenames_to_parseable_ram.h
+++
@@ -1,2 +0,0 @@
-unsigned long int extract_list_of_business_filenames_to_parseable_ram_step_one_determine_total_delmiters(FILE * list_of_businesses_dot_text);
-//int extract_list_of_business_filenames_to_parseable_ram(char ** list_of_business_records, char ** list_of_business_records_string_buffer_length, unsigned long int * list_of_business_records_total);
