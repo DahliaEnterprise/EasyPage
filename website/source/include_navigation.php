7fde60bfe3c1544b3575fa48dd1a44dbb240cc9a
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-06-21 15:14:08 -0700

    Create include_navigation.php

---
+++ website/source/include_navigation.php
@@ -0,0 +1,10 @@
+  <div class="menu_header">
+     Navigate
+    </div>
+        <div class="easy_page_header">
+     <a href="/" class="shasta_exchange_navigation">Welcome</a>
+    </div> 
+    <div class="easy_page_header">
+     <a href="/blog/index.php" class="shasta_exchange_navigation">Blog</a>
+    </div> 
+ 
