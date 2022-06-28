7537db31f5023407376d7ac8cd0f227705160279
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-06-28 13:23:58 -0700

    Update main.cpp

--- developers mind bureau/EasyPage_DahliaVideoGram/service/source/main.cpp
+++ developers mind bureau/EasyPage_DahliaVideoGram/service/source/main.cpp
@@ -1,1 +1,23 @@
 
+
+#include <stdio.h>
+#include <glib-2.0/gio/gio.h>
+#include "/usr/include/glib-2.0/glib.h"
+#include <gio-unix-2.0/gio/gunixsocketaddress.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int  main()
+{
+	GMainLoop * loop;
+	
+	loop = g_main_loop_new(0, 0);
+	
+	
+	
+	
+	
+  return 0;
+}
