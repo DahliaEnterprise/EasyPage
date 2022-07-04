7d9388e19671da7f4222c093b744668b46ad34cb
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-07-04 11:12:51 -0700

    Update build_generic_on_debian.sh

--- developers mind bureau/EasyPage_DahliaVideoGram/service/build/build_generic_on_debian.sh
+++ developers mind bureau/EasyPage_DahliaVideoGram/service/build/build_generic_on_debian.sh
@@ -1,15 +1,5 @@
-##gcc -std=c17 `pkg-config --cflags gtk+-3.0` ./../source/main.c -o main
+link_pthread="-pthread"
 
-#using echo "`pkg-config --cflags gtk+-3.0`" produced the following line"
-#-pthread -I/usr/include/gtk-3.0 -I/usr/include/at-spi2-atk/2.0 -I/usr/include/at-spi-2.0 -I/usr/include/dbus-1.0 -I/usr/lib/x86_64-linux-gnu/dbus-1.0/include -I/usr/include/gtk-3.0 -I/usr/include/gio-unix-2.0 -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/harfbuzz -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libmount -I/usr/include/blkid -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include
-
-#include headers
-gtk_threepointzero="-pthread -I/usr/include/gtk-3.0 -I/usr/include/at-spi2-atk/2.0 -I/usr/include/at-spi-2.0 -I/usr/include/dbus-1.0 -I/usr/lib/x86_64-linux-gnu/dbus-1.0/include -I/usr/include/gtk-3.0 -I/usr/include/gio-unix-2.0 -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/harfbuzz -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libmount -I/usr/include/blkid -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include"
-glib_gio_twopointzero="-I/usr/include/glib-2.0/gio"
-glib_gobject_twopointerk="-I/usr/include/glib-2.0/gobject"
-
-#link objects with compile process
-link_glib_twopointzero="-lglib-2.0"
-
+allow_line_numbers_valgrind="-g"
 #compile
-cc -std=c17 $gtk_threepointzero $glib_gio_twopointzero $glib_gobject_twopointerk ./../source/main.c -o main $link_glib_twopointzero
+gcc -std=c17 ./../source/main.c -o main $link_pthread
