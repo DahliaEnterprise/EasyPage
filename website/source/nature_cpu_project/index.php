99b372147c632bce1e889353797460caedc7b7db
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-07-05 19:52:51 -0700

    Create index.php

---
+++ website/source/nature_cpu_project/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Shasta Exchange</title>
+    <style type="text/css">
+    * {
+      margin: 0px 0px 0px 0px;
+      padding: 0px 0px 0px 0px;
+    }
+
+    body {
+     background-color: #3D6198;
+    }
+
+    #business_name {
+      font-size: 5.11em;
+      text-align: center;
+      padding: 0.5em 0.5em 0 0.5em;
+      margin: 0em 0.5em 0 0.5em;
+      font-family: PlayFairFont;
+      text-shadow: 0 0 2px #000000, 0 0 2px #000000, 0 0 5px #000000, 0 0 10px #000000; 0 0 10px #000000; 0 0 10px #000000;
+      color: #f64f4f;
+    }
+  
+   
+    div.header_quick_links {
+      text-align: center;
+      width: 99.999999%;
+      padding-top: 2.5em;
+      padding-bottom: 2.5em
+    }
+
+    #business_elevator_pitch {
+      font-size: 2.4em;
+      text-align: center;
+      font-family: PlayFairFont;
+      padding: 0 0.5em 0.5em 0.5em;
+      margin: 0.5em 0.5em 0.5em 0.5em;
+      text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e, 0 0 5px #000000, 0 0 5px #000000; 0 0 5px #000000;
+      color: #f64f4f;
+      font-weight: 900;
+    }
+
+    #logo_business_name {
+     width: 99.999999%;
+     background: url("/demo_background.jpeg");
+     background-repeat: no-repeat;
+     background-position: center 120px;
+     padding-bottom: 200px;
+     border-bottom: 3px solid #f64f4f;
+    }
+
+    @font-face {
+     font-family: PlayFairFont;
+     src: url("/externally_provided/fonts/PlayFair_Display/PlayfairDisplaySC-Regular.otf");
+    }
+
+    @font-face {
+     font-family: MochiPopOne;
+     src: url("/externally_provided/fonts/mochiypop/fonts/ttf/MochiyPopOne-Regular.tff");
+    }
+
+    .easy_page_header {
+     text-align: center;
+     
+     padding: 1em;
+     margin: 0 2em 1em 2em;
+     border-radius: 10px;	
+     background: #008CBA;
+     box-shadow: 3px 3px 5px #000000;
+    }
+
+    a.shasta_exchange_navigation:link {
+    color:#ffffff;
+     text-align: center;
+     font-size: 3.5em;
+     text-decoration: none;
+     
+    }
+
+    a.shasta_exchange_navigation:visited {
+     font-family: MochiPopOne;
+     color: #ffffff;
+     text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e;
+     text-decoration: none;
+    }
+
+    .menu_header {
+     text-align: center;
+     font-size: 3.5em;
+     font-family: PlayFairFont;
+     text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e;
+      color: #f64f4f;
+      font-weight: 900;
+    }
+	
+    .blog_introduction {
+     border-radius: 10px;
+     background: #008CBA;
+     border: 3px solid #000000;
+     margin: 0 0.5em 0.5em 0.5em;
+     font-size: 3.5em;
+     color: #ffffff;
+     padding: 1em;
+    }
+    </style>
+  </head>
+  <body>
+    <div id="logo_business_name">
+      <h1 id="business_name">Shasta Exchange</h1>
+      <h2 id="business_elevator_pitch">Digital based printed media</h2>
+    </div>
+    
+   
+      <?php include('./../include_navigation.php'); ?>
+    <div style="margin: 3em;">&nbsp;</div>
+    <div class="menu_header">
+     Nature CPU
+    </div>
+    <div class="blog_introduction">
+    &nbsp;&nbsp;&nbsp;&nbsp;The nature cpu is to achieve a solid interworking of outputs as opposed to a set of limited features such as arithmatic(add,suntract,divide,multiply) and floating point(decimals) capabilities. The problem faced today with CPUs is core functionality is virtualized as a function within the core operating system and then protected from being overwritten by having a checking system(permissions) from overwriting these functions in irdee to garuntee to the operator that certain operating conditions will exist such as when you add two plus two you get four(and more importantly writing to the harddrive as requested). <br/><br/>  This leads to everyone creating a plethora of assembly editors,styles, sometimes the assembly editors have their own assembly language makeup that is inconsistent with the transistor switching within its soecification if cpu however still complies and usualt works, then tou have the worst problem of all, everyone technically has their own C language makeup, including compiler options and the way object files are translated, with that in mind a developer from a company is trying to explain how to code with their library when technically they canr do anything with out "doing the work" for you which becomes a licenseing and copyright territory.<br/><br/>
+    Nature CPU instead creates hardware modules that are independent from each other. To be clear a specific module will be used for specifically arthmetic, while a specific module will be used for concatenation of strings; Anothee module used only for storing and handling video pixel data(not to be confused with a graphics processor), another module specifically for feeding data onto a monitor and another module for specifically managing the coordinates of a two dimensions, and so on. <br/><br/>This is differenr because now instead of proverbially fighting and living a mentally imflamatory nightmare, now developers will invoke a signal number with parameters to concat a string as opposed to asking the operating system developers for permission by means of location finding where that virtualization exists.<br/><br/>This has many benefits that can come from down the road, economically it can create jobs by allowing replacement of modules with peoples own ideas on how their transistor organization might be more effective and or efficient then factory modules, this also more importantly prevents confusion as to what is occuring within the hardware, and my favorite is that is allows a person to physically look at the hardware and know what is capable of, this could reduce sezuires from law enforcment if their is a similar process of looking a paper documents and seeing that there is no evidence of illegal activities where virtualized capabilities it costs millions to train cyber security experts to be ready for any digital setup to seize and inspect the data and function parameters. Law enforcment issue reduction aside it also allows for at home repairs along with physical inspections for administration to look at their hardware and see that nothinng has changed as opposed to virtualized functions it IS IMPOSSIBLE to ever know what your computing is doing while you are not look and while you are looking.
+    
+    </div>
+    
+    
+    
+     
+    
+    <div style="margin:3em;"></div> 
+    <div style="margin:0.5em;text-align:center; color:#ffffff; font-size:2.5em;">Page created by Shane Bryan Betz</div>
+    <div style="margin:6em;"></div>
+   </body>
+  </html>
