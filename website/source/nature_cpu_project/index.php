01af56147b47621ed6240f352cdbdc0f5f5025c1
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-07-13 19:47:35 -0700

    Update index.php

--- website/source/nature_cpu_project/index.php
+++ website/source/nature_cpu_project/index.php
@@ -4,98 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Shasta Exchange</title>
     <style type="text/css">
-    * {
-      margin: 0px 0px 0px 0px;
-      padding: 0px 0px 0px 0px;
-    }
-
-    body {
-     background-color: #3D6198;
-    }
-
-    #business_name {
-      font-size: 5.11em;
-      text-align: center;
-      padding: 0.5em 0.5em 0 0.5em;
-      margin: 0em 0.5em 0 0.5em;
-      font-family: PlayFairFont;
-      text-shadow: 0 0 2px #000000, 0 0 2px #000000, 0 0 5px #000000, 0 0 10px #000000; 0 0 10px #000000; 0 0 10px #000000;
-      color: #f64f4f;
-    }
-  
-   
-    div.header_quick_links {
-      text-align: center;
-      width: 99.999999%;
-      padding-top: 2.5em;
-      padding-bottom: 2.5em
-    }
-
-    #business_elevator_pitch {
-      font-size: 2.4em;
-      text-align: center;
-      font-family: PlayFairFont;
-      padding: 0 0.5em 0.5em 0.5em;
-      margin: 0.5em 0.5em 0.5em 0.5em;
-      text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e, 0 0 5px #000000, 0 0 5px #000000; 0 0 5px #000000;
-      color: #f64f4f;
-      font-weight: 900;
-    }
-
-    #logo_business_name {
-     width: 99.999999%;
-     background: url("/demo_background.jpeg");
-     background-repeat: no-repeat;
-     background-position: center 120px;
-     padding-bottom: 200px;
-     border-bottom: 3px solid #f64f4f;
-    }
-
-    @font-face {
-     font-family: PlayFairFont;
-     src: url("/externally_provided/fonts/PlayFair_Display/PlayfairDisplaySC-Regular.otf");
-    }
-
-    @font-face {
-     font-family: MochiPopOne;
-     src: url("/externally_provided/fonts/mochiypop/fonts/ttf/MochiyPopOne-Regular.tff");
-    }
-
-    .easy_page_header {
-     text-align: center;
-     
-     padding: 1em;
-     margin: 0 2em 1em 2em;
-     border-radius: 10px;	
-     background: #008CBA;
-     box-shadow: 3px 3px 5px #000000;
-    }
-
-    a.shasta_exchange_navigation:link {
-    color:#ffffff;
-     text-align: center;
-     font-size: 3.5em;
-     text-decoration: none;
-     
-    }
-
-    a.shasta_exchange_navigation:visited {
-     font-family: MochiPopOne;
-     color: #ffffff;
-     text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e;
-     text-decoration: none;
-    }
-
-    .menu_header {
-     text-align: center;
-     font-size: 3.5em;
-     font-family: PlayFairFont;
-     text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e;
-      color: #f64f4f;
-      font-weight: 900;
-    }
-	
-    .blog_introduction {
+       .about_the_company {
      border-radius: 10px;
      background: #008CBA;
      border: 3px solid #000000;
@@ -105,6 +14,10 @@
      padding: 1em;
     }
     </style>
+  
+    
+    <link rel="stylesheet" href="/include_stylesheet.css"/>
+    
   </head>
   <body>
     <div id="logo_business_name">
