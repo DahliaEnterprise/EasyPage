e901cf49990329351de2a2bf836cdb38c44aeb75
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-07-13 19:48:24 -0700

    Update index.php

--- website/source/password_generator/index.php
+++ website/source/password_generator/index.php
@@ -4,98 +4,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Shasta Exchange</title>
     <style type="text/css">
-    * {
-      margin: 0px 0px 0px 0px;
-      padding: 0px 0px 0px 0px;
-    }
-
-    body {
-     background-color: #3D6198;
-    }
-
-    #business_name {
-      font-size: 5.11em;
-      text-align: center;
-      padding: 0.5em 0.5em 0 0.5em;
-      margin: 0em 0.5em 0 0.5em;
-      font-family: PlayFairFont;
-      text-shadow: 0 0 2px #000000, 0 0 2px #000000, 0 0 5px #000000, 0 0 10px #000000; 0 0 10px #000000; 0 0 10px #000000;
-      color: #f64f4f;
-    }
-  
-   
-    div.header_quick_links {
-      text-align: center;
-      width: 99.999999%;
-      padding-top: 2.5em;
-      padding-bottom: 2.5em
-    }
-
-    #business_elevator_pitch {
-      font-size: 2.4em;
-      text-align: center;
-      font-family: PlayFairFont;
-      padding: 0 0.5em 0.5em 0.5em;
-      margin: 0.5em 0.5em 0.5em 0.5em;
-      text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e, 0 0 5px #000000, 0 0 5px #000000; 0 0 5px #000000;
-      color: #f64f4f;
-      font-weight: 900;
-    }
-
-    #logo_business_name {
-     width: 99.999999%;
-     background: url("/demo_background.jpeg");
-     background-repeat: no-repeat;
-     background-position: center 120px;
-     padding-bottom: 200px;
-     border-bottom: 3px solid #f64f4f;
-    }
-
-    @font-face {
-     font-family: PlayFairFont;
-     src: url("/externally_provided/fonts/PlayFair_Display/PlayfairDisplaySC-Regular.otf");
-    }
-
-    @font-face {
-     font-family: MochiPopOne;
-     src: url("/externally_provided/fonts/mochiypop/fonts/ttf/MochiyPopOne-Regular.tff");
-    }
-
-    .easy_page_header {
-     text-align: center;
-     
-     padding: 1em;
-     margin: 0 2em 1em 2em;
-     border-radius: 10px;	
-     background: #008CBA;
-     box-shadow: 3px 3px 5px #000000;
-    }
-
-    a.shasta_exchange_navigation:link {
-    color:#ffffff;
-     text-align: center;
-     font-size: 3.5em;
-     text-decoration: none;
-     
-    }
-
-    a.shasta_exchange_navigation:visited {
-     font-family: MochiPopOne;
-     color: #ffffff;
-     text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e;
-     text-decoration: none;
-    }
-
-    .menu_header {
-     text-align: center;
-     font-size: 3.5em;
-     font-family: PlayFairFont;
-     text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e;
-      color: #f64f4f;
-      font-weight: 900;
-    }
-	
-    .blog_introduction {
+        <style type="text/css">
+       .about_the_company {
      border-radius: 10px;
      background: #008CBA;
      border: 3px solid #000000;
@@ -105,6 +15,133 @@
      padding: 1em;
     }
     </style>
+  
+    
+    <link rel="stylesheet" href="/include_stylesheet.css"/>
+    
+		<script>
+		 var sha256 = function sha256(ascii) {
+	function rightRotate(value, amount) {
+		return (value>>>amount) | (value<<(32 - amount));
+	};
+	
+	var mathPow = Math.pow;
+	var maxWord = mathPow(2, 32);
+	var lengthProperty = 'length'
+	var i, j; // Used as a counter across the whole file
+	var result = ''
+
+	var words = [];
+	var asciiBitLength = ascii[lengthProperty]*8;
+	
+	//* caching results is optional - remove/add slash from front of this line to toggle
+	// Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
+	// (we actually calculate the first 64, but extra values are just ignored)
+	var hash = sha256.h = sha256.h || [];
+	// Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
+	var k = sha256.k = sha256.k || [];
+	var primeCounter = k[lengthProperty];
+	/*/
+	var hash = [], k = [];
+	var primeCounter = 0;
+	//*/
+
+	var isComposite = {};
+	for (var candidate = 2; primeCounter < 64; candidate++) {
+		if (!isComposite[candidate]) {
+			for (i = 0; i < 313; i += candidate) {
+				isComposite[i] = candidate;
+			}
+			hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
+			k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
+		}
+	}
+	
+	ascii += '\x80' // Append Ƈ' bit (plus zero padding)
+	while (ascii[lengthProperty]%64 - 56) ascii += '\x00' // More zero padding
+	for (i = 0; i < ascii[lengthProperty]; i++) {
+		j = ascii.charCodeAt(i);
+		if (j>>8) return; // ASCII check: only accept characters in range 0-255
+		words[i>>2] |= j << ((3 - i)%4)*8;
+	}
+	words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
+	words[words[lengthProperty]] = (asciiBitLength)
+	
+	// process each chunk
+	for (j = 0; j < words[lengthProperty];) {
+		var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
+		var oldHash = hash;
+		// This is now the undefinedworking hash", often labelled as variables a...g
+		// (we have to truncate as well, otherwise extra entries at the end accumulate
+		hash = hash.slice(0, 8);
+		
+		for (i = 0; i < 64; i++) {
+			var i2 = i + j;
+			// Expand the message into 64 words
+			// Used below if 
+			var w15 = w[i - 15], w2 = w[i - 2];
+
+			// Iterate
+			var a = hash[0], e = hash[4];
+			var temp1 = hash[7]
+				+ (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
+				+ ((e&hash[5])^((~e)&hash[6])) // ch
+				+ k[i]
+				// Expand the message schedule if needed
+				+ (w[i] = (i < 16) ? w[i] : (
+						w[i - 16]
+						+ (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
+						+ w[i - 7]
+						+ (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
+					)|0
+				);
+			// This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
+			var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
+				+ ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
+			
+			hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
+			hash[4] = (hash[4] + temp1)|0;
+		}
+		
+		for (i = 0; i < 8; i++) {
+			hash[i] = (hash[i] + oldHash[i])|0;
+		}
+	}
+	
+	for (i = 0; i < 8; i++) {
+		for (j = 3; j + 1; j--) {
+			var b = (hash[i]>>(j*8))&255;
+			result += ((b < 16) ? 0 : '') + b.toString(16);
+		}
+	}
+	return result;
+};
+function compute()
+{
+	
+  var rounds = document.getElementById("password_hash_multiplier").value; //a number you won't forget that is not your known favorite number within a range of 10 and 1,000,000
+	var index = 0;
+	var input = document.getElementById("password_input_one").value;
+	var last_result = input;
+	while(index <=	rounds)
+	{
+		last_result = last_result + input;
+		
+		index = index + 1;
+	
+	}
+	
+	index = 0;
+	while(index <= 5000)
+	{
+		last_result = sha256(last_result);
+		
+		index = index + 1;
+	}
+	
+	document.getElementById("password_result").innerHTML = sha256(last_result);
+}
+  </script>
   </head>
   <body>
     <div id="logo_business_name">
@@ -126,17 +163,15 @@
     
     <form action="javascript:preventDefault();">
     <h3>A memorable pass phrase</h3>
-     <textarea id="password_input_one"  style="height:2em;font-size:100%;">
-     </textarea>
+     <input id="password_input_one"  style="height:2em;font-size:100%;" />
+    
      <br/><br/>
         <h3>pick and remember this number between one hundred and one million</h3>
-        <div>larger numbers may take slightly longer to compute and is used to concat your input pass phrase that many times as a defense against rainbow tables(customer data leaks may not affect you in most cases due to this large and randomized salt).
-     <textarea id="password_hash_multiplier_one"  style="font-size:100%;">
-     100
-     </textarea>
-     
+        <div>larger numbers may take slightly longer to compute and is used to concat your input pass phrase that many times as a defense against rainbow tables(customer data leaks may not affect you in most cases due to this large and randomized salt).</div>
+     <textarea id="password_hash_multiplier"  style="font-size:100%;">100</textarea>
+     <a href="javascript: void();" onMouseUp="compute();">Generate Reproduceable Passphrase</a>
     </form> 
-    
+    <div id="password_result">Your password will appear here</div>
     </div>
     
     
@@ -145,4 +180,4 @@
     <div style="margin:0.5em;text-align:center; color:#ffffff; font-size:2.5em;">Page created by Shane Bryan Betz</div>
     <div style="margin:6em;"></div>
    </body>
-  </html>
+	 </html>
