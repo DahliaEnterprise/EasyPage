c0f7da2ff41f8b8a423923360d0f8323f4500a08
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-07-13 19:44:25 -0700

    Update index.php

--- website/source/blog/index.php
+++ website/source/blog/index.php
@@ -4,97 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Shasta Exchange</title>
     <style type="text/css">
-    * {
-      margin: 0px 0px 0px 0px;
-      padding: 0px 0px 0px 0px;
-    }
-
-    body {
-     background-color: #3D6198;
-    }
-
-    #business_name {
-      font-size: 5.11em;
-      text-align: center;
-      padding: 0.5em 0.5em 0 0.5em;
-      margin: 0em 0.5em 0 0.5em;
-      font-family: PlayFairFont;
-      text-shadow: 0 0 2px #000000, 0 0 2px #000000, 0 0 5px #000000, 0 0 10px #000000; 0 0 10px #000000; 0 0 10px #000000;
-      color: #f64f4f;
-    }
-  
-   
-    div.header_quick_links {
-      text-align: center;
-      width: 99.999999%;
-      padding-top: 2.5em;
-      padding-bottom: 2.5em
-    }
-
-    #business_elevator_pitch {
-      font-size: 2.4em;
-      text-align: center;
-      font-family: PlayFairFont;
-      padding: 0 0.5em 0.5em 0.5em;
-      margin: 0.5em 0.5em 0.5em 0.5em;
-      text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e, 0 0 5px #000000, 0 0 5px #000000; 0 0 5px #000000;
-      color: #f64f4f;
-      font-weight: 900;
-    }
-
-    #logo_business_name {
-     width: 99.999999%;
-     background: url("/demo_background.jpeg");
-     background-repeat: no-repeat;
-     background-position: center 120px;
-     padding-bottom: 200px;
-     border-bottom: 3px solid #f64f4f;
-    }
-
-    @font-face {
-     font-family: PlayFairFont;
-     src: url("/externally_provided/fonts/PlayFair_Display/PlayfairDisplaySC-Regular.otf");
-    }
-
-    @font-face {
-     font-family: MochiPopOne;
-     src: url("/externally_provided/fonts/mochiypop/fonts/ttf/MochiyPopOne-Regular.tff");
-    }
-
-    .easy_page_header {
-     text-align: center;
-     
-     padding: 1em;
-     margin: 0 2em 1em 2em;
-     border-radius: 10px;	
-     background: #008CBA;
-     box-shadow: 3px 3px 5px #000000;
-    }
-
-    a.shasta_exchange_navigation:link {
-     text-align: center;
-     font-size: 3.5em;
-     text-decoration: none;
-     
-    }
-
-    a.shasta_exchange_navigation:visited {
-     font-family: MochiPopOne;
-     color: #ffffff;
-     text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e;
-     text-decoration: none;
-    }
-
-    .menu_header {
-     text-align: center;
-     font-size: 3.5em;
-     font-family: PlayFairFont;
-     text-shadow: 0 0 2px #7d1e3e, 0 0 2px #7d1e3e, 0 0 3px #7d1e3e;
-      color: #f64f4f;
-      font-weight: 900;
-    }
-	
-    .blog_introduction {
+       .about_the_company {
      border-radius: 10px;
      background: #008CBA;
      border: 3px solid #000000;
@@ -104,6 +14,9 @@
      padding: 1em;
     }
     </style>
+  
+    
+    <link rel="stylesheet" href="/include_stylesheet.css"/>
   </head>
   <body>
     <div id="logo_business_name">
@@ -112,26 +25,13 @@
     </div>
     
    
-    <div class="menu_header">
-     Navigate
-    </div>
-        <div class="easy_page_header">
-     <a href="/" class="shasta_exchange_navigation">Welcome</a>
-    </div> 
-    <div class="easy_page_header">
-     <a href="/blog/index.php" class="shasta_exchange_navigation">Blog</a>
-    </div> 
-    <!--
-    <div class="easy_page_header">
-     <a href="" class="shasta_exchange_navigation">Financial Planner</a>
-    </div>
-    -->
+    <?php include('/usr/local/apache2/htdocs/include_navigation.php'); ?>
     <div style="margin: 3em;">&nbsp;</div>
     <div class="menu_header">
      What you can expect from our blog
     </div>
     <div class="blog_introduction">
-     &nbsp;&nbsp;&nbsp;&nbsp;This is the Shasta Exchange blog, here you can retrieve updates about the company. The blog will also feature tutorials that may be able to help you become more aware and adept at building things. This blog will also include miscellaneous projects that usually intended for the customer(probably you) however also demonstrate the skill set of the shasta exchange company.
+     &nbsp;&nbsp;&nbsp;&nbsp;This is the Shasta Exchange blog, here you can retrieve updates about the company. The blog will also feature tutorials that may be able to help you become more aware and adept at building things. <br/><br/>This blog will also include miscellaneous projects that are usually intended for the customer(probably you) however also demonstrate the skill set of the shasta exchange company.
     </div>
     <div style="margin:3em;"></div> 
     <div style="margin:0.5em;text-align:center; color:#ffffff; font-size:2.5em;">Page created by Shane Bryan Betz</div>
