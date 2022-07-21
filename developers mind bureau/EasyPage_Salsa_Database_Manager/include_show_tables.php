300fb7c7c1b445e38d85b1e55fdf7e2af62085ba
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-07-21 14:41:06 -0700

    Create include_show_tables.php

---
+++ developers mind bureau/EasyPage_Salsa_Database_Manager/include_show_tables.php
@@ -0,0 +1,38 @@
+<?php
+ 	$dsn = "mysql:host=127.0.0.1;dbname=".$_POST["database_name"].";charset=utf8mb4";
+						$options = [
+						    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+						    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+						    PDO::ATTR_EMULATE_PREPARES   => false,
+						    PDO::ATTR_PERSISTENT => false
+						];
+						try {
+						     $connection = new PDO($dsn, $_POST["database_username"], $_POST["database_password"], $options);
+						} catch (PDOException $e) {
+							var_dump($e);
+						   //  throw new PDOException($e->getMessage(), (int)$e->getCode());
+						}
+						try{
+ 						$query_in_progress = $connection->query($_POST["select_query"]);
+					}catch(PDOException $e)
+						{
+							var_dump($e);
+						}
+						$query_in_progress->execute();
+						
+						$keep_fetching = 1;
+						while($keep_fetching == 1)
+						{
+								$fetched = $query_in_progress->fetch();
+								
+								if($fetched == false)
+								{
+										$keep_fetching = 0;
+								}else{
+									//echo $fetched["Database"]."<br/>";
+									var_dump($_POST["select_query"]);
+								}
+								
+								
+							}
+?>
