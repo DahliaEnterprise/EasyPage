cac2bf29fcf45190ea7d4fc38df1cf804b045cdd
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-07-21 14:39:19 -0700

    Update include_authorization.php

--- developers mind bureau/EasyPage_Salsa_Database_Manager/include_authorization.php
+++ developers mind bureau/EasyPage_Salsa_Database_Manager/include_authorization.php
@@ -6,11 +6,11 @@ if(isset($_POST["token"]) == true)
 {
 	$token = $_POST["token"];
 }
-$token_to_match = "b81bf90b90152555d2f1caeacef0d8aa60f849941df07c211898a6e64e94456f";
+$token_to_match = "";
 
 if(isset($_POST["password"]) == true)
 {
-	if(hash_equals($_POST["password"], "3cecc61248fda100eaf2f2b65dbfee73dd41d386de829ba7b1f5c7b16122afc5") == true)
+	if(hash_equals($_POST["password"], "") == true)
 	{
 		usleep(rand(500, 3000));
 		if(isset($_POST["username"]) == true)
